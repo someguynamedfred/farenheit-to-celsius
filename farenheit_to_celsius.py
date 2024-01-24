4eff25770c123f3e8760583fdee765dd413479c1
someguynamedfred <134651167+someguynamedfred@users.noreply.github.com>
2024-01-24 13:44:22 -0500

    Update farenheit_to_celsius.py

--- farenheit_to_celsius.py
+++ farenheit_to_celsius.py
@@ -1,7 +1,15 @@
 # Fahrenheit to Celsius converter
-def f_to_c(fahrenheit):
-    celsius = (5 / 9 * (farenheit) - 32)
-    return(int(celsius))
+def f_to_c(farenheit):
+    celsius = (5/9*(int(farenheit))-32)
+    return(celsius)
 
 farenheit=input("What is the f? ")
+
+#I want this in the function above but the function isn't working
+celsius = (5/9*(int(farenheit))-32)
+
+#this should print the function
+print(int(celsius))
+
+#this isn't working
 print(f_to_c(celsius))
