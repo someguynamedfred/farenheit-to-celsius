fb44b366c43f0b929b75b70ccfcf2f9e05fa06df
someguynamedfred <134651167+someguynamedfred@users.noreply.github.com>
2024-01-24 13:36:11 -0500

    Create farenheit_to_celsius.py

---
+++ farenheit_to_celsius.py
@@ -0,0 +1,7 @@
+# Fahrenheit to Celsius converter
+def f_to_c(fahrenheit):
+    celsius = (5 / 9 * (farenheit) - 32)
+    return(int(celsius))
+
+farenheit=input("What is the f? ")
+print(f_to_c(celsius))
