3c19ff1f2517d99a28e62a48cec98c35fa99cbd9
someguynamedfred <134651167+someguynamedfred@users.noreply.github.com>
2024-01-24 16:22:06 -0500

    Update farenheit_to_celsius.py
    got it working both ways now. Just needs some polish now.

--- farenheit_to_celsius.py
+++ farenheit_to_celsius.py
@@ -1,12 +1,18 @@
 # Fahrenheit to Celsius converter
-def f_to_c(farenheit):
-    celsius = (5 / 9 * (int(farenheit)) - 32)
+def f_to_c(temperature):
+    celsius = (5 / 9 * (int(temperature)) - 32)
     return(int(celsius))
+    
+def c_to_f(temperature):
+    farenheit = ((int(temperature) * 1.8) + 32)
+    return(int(farenheit))
 
-farenheit = input("What is the f? ")
+#temperature = input("What is the f? ")
 #celsius = (5/9*(int(farenheit))-32)
-print("That converts to " + str(f_to_c(farenheit)) + " degrees Celsius.")
+#print("That converts to " + str(f_to_c(temperature)) + " degrees Celsius.")
 
-
-loop == False
-while loop = False
+temperature, farenheit_or_celsius = input("What is the temperature number and unit you want to convert from? (eg. 150 C) ") .split()
+if farenheit_or_celsius == "c":
+    print(c_to_f(temperature))
+if farenheit_or_celsius == "f":
+    print(f_to_c(temperature))
