7c317f6604d4a5ae9f618189932fd29af083a6d8
someguynamedfred <134651167+someguynamedfred@users.noreply.github.com>
2024-01-26 11:21:41 -0500

    Update farenheit_to_celsius.py
    forgot a parentheses

--- farenheit_to_celsius.py
+++ farenheit_to_celsius.py
@@ -4,7 +4,7 @@ def f_to_c(temperature):
     return(int(celsius))
 #for the sake of learning, here is a simpler code
 #def f_to_c(temperature):
-#    return ((temperature) - 32)*5/9)
+#    return (((temperature) - 32)*5/9)
     
 def c_to_f(temperature):
     farenheit = ((int(temperature) * 1.8) + 32)
