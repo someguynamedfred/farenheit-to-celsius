ce1df9a49d8e09a6293d9baf3d0853dbb7c6afb6
someguynamedfred <134651167+someguynamedfred@users.noreply.github.com>
2024-01-26 11:20:56 -0500

    Update farenheit_to_celsius.py
    made a note about how to code this simpler

--- farenheit_to_celsius.py
+++ farenheit_to_celsius.py
@@ -2,6 +2,9 @@
 def f_to_c(temperature):
     celsius = (5 / 9 * ((int(temperature)) - 32))
     return(int(celsius))
+#for the sake of learning, here is a simpler code
+#def f_to_c(temperature):
+#    return ((temperature) - 32)*5/9)
     
 def c_to_f(temperature):
     farenheit = ((int(temperature) * 1.8) + 32)
